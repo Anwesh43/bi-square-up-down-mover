89e213c61552a27d9f78b83ec64a86ea16b39235
Anwesh43 <anweshthecool0@gmail.com>
2020-05-13 06:05:10 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import {sinify, divideScale} from './hooks'
 
 export const useAnimatedScale = (scGap, delay) => {
     const [scale, setScale] = useState(0)
@@ -40,5 +41,35 @@ export const useDimension = () => {
     return {
         w,
         h
+    }
+}
+
+export const useStyle = (w, h, scale) => {
+    const sf = sinify(scale)
+    const size = Math.min(w, h) / 6
+    return {
+        getBlockStyle(i) {
+            const sf1 = divideScale(sf, 0, 2)
+            const sf2 = divideScale(sf, 1, 2)
+            const sf1i = divideScale(sf1, i, 2)
+            const sf21 = divideScale(sf2, i, 2)
+            const si = 1 - 2 * i
+            const x = (w - size) * i + (w / 2 - size) * sf1i * si
+            const y = h / 2 + (h / 2 - size) * sf2i * si
+            const left = `${x}px`
+            const top = `${y}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const position = 'absolute'
+            const background = '#3F51B5'
+            return {
+                left,
+                top,
+                position,
+                width,
+                height,
+                background
+            }
+        }
     }
 }
